6d5bcd42b9548e5faf60da8b848e3fc4f5831efa
Emmanuel Biose <slimbion@gmail.com>
2020-04-03 18:06:13 +0100

    1. Implemented Departure board UI

--- components/train_departure_board.js
+++ components/train_departure_board.js
@@ -14,6 +14,7 @@ const TrainDepartureBoardScreen = ({navigation}) => {
   const [station, setStation] = useState('');
   const [stationsSuggestions, setStationsSuggestions] = useState([]);
   const [departures, updateDepartures] = useState([]);
+  const [isRefreshing, toggleIsRefreshing] = useState(false);
 
   useEffect(() => {
     async function getLiveTrainUpdates() {}
@@ -42,6 +43,7 @@ const TrainDepartureBoardScreen = ({navigation}) => {
    * @returns {Promise<void>}
    */
   const fetchDepartures = async () => {
+    toggleIsRefreshing(true);
     if (station !== '') {
       fetch(`http://esrs.herokuapp.com/api/departures/${station.toLowerCase()}`)
         .then(response => response.json())
@@ -51,8 +53,10 @@ const TrainDepartureBoardScreen = ({navigation}) => {
             console.log(departure);
             return {
               id: `${++count}`,
-              depart: departure.origin[0].locationName,
-              dest: departure.destination[0].locationName,
+              departName: departure.origin[0].locationName,
+              destName: departure.destination[0].locationName,
+              departCRS: departure.origin[0].crs,
+              destCRS: departure.destination[0].crs,
               std: departure.std,
               etd: departure.etd,
               platform: departure.platform,
@@ -61,6 +65,7 @@ const TrainDepartureBoardScreen = ({navigation}) => {
               delayReason: departure.delayReason,
             };
           });
+          toggleIsRefreshing(false);
           updateDepartures(departures);
         });
     }
@@ -68,21 +73,28 @@ const TrainDepartureBoardScreen = ({navigation}) => {
 
   return (
     <View style={styles.root}>
-      <View style={styles.seeTravelsContainer}>
-        <View style={styles.viewSeeTravels}>
-          <Text style={styles.textSeeTravels}>Departures</Text>
-        </View>
+      <View style={styles.navBar}>
+        <Text style={styles.navBarText}>Departures</Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.reset({
+              index: 0,
+              routes: [{name: 'Tickets'}],
+            });
+          }}>
+          <Text style={styles.closeButton}>close</Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.ticketsSearchIconContainer}>
+      <View style={styles.searchIconContainer}>
         <TextInput
           value={station}
           placeholder="Station"
-          style={styles.textInputTicketsSearch}
+          style={styles.searchInput}
           onChangeText={text => searchStation(text)}
         />
         <TouchableOpacity
           onPress={() => fetchDepartures()}
-          style={styles.ticketsSearchIcon}>
+          style={styles.searchIcon}>
           <Image source={require('../resources/search.png')} />
         </TouchableOpacity>
         <FlatList
@@ -109,15 +121,38 @@ const TrainDepartureBoardScreen = ({navigation}) => {
         ListEmptyComponent={() => (
           <Image source={require('../resources/empty_state.png')} />
         )}
+        refreshing={isRefreshing}
+        onRefresh={() => fetchDepartures()}
         renderItem={({item, index}) => (
-          <View style={[styles.journeyView, item.style]}>
+          <View style={[styles.departureView, item.style]}>
             <View style={styles.journeyDetails}>
-              <Text>{item.depart}</Text>
-              <Text>{item.dest}</Text>
-              <Text>{item.platform}</Text>
-              <Text>{item.operator}</Text>
-              <Text>{item.cancelReason}</Text>
-              <Text>{item.delayReason}</Text>
+              <View style={styles.dotsContainer}>
+                <Text style={styles.departCrs}>°</Text>
+                <View style={styles.dots}>
+                  <View style={styles.dot} />
+                  <View style={styles.dot} />
+                  <View style={styles.dot} />
+                  <View style={styles.dot} />
+                  <View style={styles.dot} />
+                </View>
+                <Text style={styles.arriveCrs}>°</Text>
+              </View>
+              <View style={styles.departureDetailContainer}>
+                <View style={styles.departureDetails}>
+                  <Text style={styles.departureText}>DEPARTURE</Text>
+                  <Text style={styles.LocationName}>{item.departName}</Text>
+                  <Text style={styles.boldDetails}>
+                    ESTIMATED TIME: {item.etd}
+                  </Text>
+                  <Text style={styles.boldDetails}>
+                    PLATFORM: {item.platform}
+                  </Text>
+                </View>
+                <View style={styles.departureDetails}>
+                  <Text style={styles.arrivalText}>ARRIVAL</Text>
+                  <Text style={styles.LocationName}>{item.destName}</Text>
+                </View>
+              </View>
             </View>
           </View>
         )}
@@ -134,30 +169,27 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(104, 126, 252, 0.1)',
     ...StyleSheet.absoluteFillObject,
   },
-  seeTravelsContainer: {
+  navBar: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  viewSeeTravels: {
-    marginBottom: 10,
-  },
-  textSeeTravels: {
+  navBarText: {
     fontWeight: 'bold',
     fontSize: 30,
     color: '#190320',
     fontFamily: 'sans-serif-thin',
   },
-  ticketsSearchIconContainer: {
+  searchIconContainer: {
     height: 60,
     marginTop: 10,
   },
-  ticketsSearchIcon: {
+  searchIcon: {
     position: 'absolute',
     right: 20,
     top: 20,
   },
-  textInputTicketsSearch: {
+  searchInput: {
     backgroundColor: '#FFFFFF',
     borderColor: '#CCCCCC',
     borderRadius: 8,
@@ -173,26 +205,19 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: '#000000',
   },
-  journeyView: {
+  departureView: {
     flexDirection: 'row',
     marginTop: 10,
-    height: 80,
-  },
-  journeyViewSelected: {
-    opacity: 0.5,
   },
   journeyDetails: {
     backgroundColor: '#FFFFFF',
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-    borderTopRightRadius: 6,
-    borderBottomRightRadius: 6,
-    padding: 10,
+    borderRadius: 5,
+    padding: 5,
     margin: 1,
     flex: 1,
     color: '#000000',
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     alignContent: 'center',
   },
@@ -202,4 +227,63 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   emptyStateNull: {},
+  dots: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  dot: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    margin: 5,
+  },
+  dotsContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  arriveCrs: {
+    color: '#CCCCCC',
+    fontWeight: 'bold',
+  },
+  departCrs: {
+    fontWeight: 'bold',
+    color: '#373759',
+  },
+  departureDetails: {
+    flexDirection: 'column',
+    padding: 10,
+    marginLeft: 15,
+  },
+  departureText: {
+    fontWeight: 'bold',
+    fontSize: 10,
+    color: '#373759',
+  },
+  arrivalText: {
+    fontWeight: 'bold',
+    fontSize: 10,
+    color: '#CCCCCC',
+  },
+  LocationName: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    paddingTop: 3,
+    letterSpacing: 1,
+  },
+  departureDetailContainer: {
+    flexDirection: 'column',
+  },
+  closeButton: {
+    backgroundColor: '#687DFC',
+    borderRadius: 8,
+    color: '#FFFFFF',
+    padding: 8,
+    textAlign: 'center',
+  },
+  boldDetails: {
+    fontWeight: 'bold',
+    fontSize: 11,
+    color: '#687DFC',
+  },
 });
